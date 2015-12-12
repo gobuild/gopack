25e046620d45091cbda44d82f404c6748db3a774
shengxiang <hzsunshx@corp.netease.com>
2015-12-12 23:24:29 +0800

    update gopack, support gopack install codeskyblue/bee

--- cmds/install.go
+++ cmds/install.go
@@ -6,14 +6,16 @@ import (
 	"os/exec"
 	"path/filepath"
 	"runtime"
+	"strings"
 
 	"github.com/codegangsta/cli"
 	"github.com/qiniu/log"
 )
 
 var InstallFlag = cli.Command{
-	Name:  "install",
-	Usage: fmt.Sprintf("Install new binary"),
+	Name:    "install",
+	Aliases: []string{"i"},
+	Usage:   fmt.Sprintf("Install binary from http://gobuild.io"),
 	Flags: []cli.Flag{
 		cli.BoolFlag{
 			Name:  "debug",
@@ -57,11 +59,19 @@ func getInsPath(names ...string) string {
 // http://dn-gobuild5.qbox.me/gorelease/gorelease/gorelease/master/darwin-amd64/gorelease.zip
 // Need to update
 func downloadSource(name string) (dest string, err error) {
+	parts := strings.Split(name, "/")
+	if len(parts) != 2 {
+		return "", fmt.Errorf("name: %s can only contains on /", name)
+	}
+
+	owner, repo := parts[0], parts[1]
 	osarch := runtime.GOOS + "-" + runtime.GOARCH
-	url := fmt.Sprintf("http://dn-gobuild5.qbox.me/gorelease/codeskyblue/%s/master/%s/%s.zip",
-		name, osarch, name)
+	url := fmt.Sprintf("http://dn-gobuild5.qbox.me/gorelease/%s/%s/master/%s/%s.zip",
+		owner, repo, osarch, repo)
+
+	prompt("Downloading %v", url)
 	log.Debug("download:", url)
-	dest = getInsPath("src", fmt.Sprintf("%s.zip", name))
+	dest = getInsPath("src", fmt.Sprintf("%s.zip", repo))
 	cmd := exec.Command("curl", "-fSL", url, "-o", dest)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
@@ -77,10 +87,18 @@ func deployPackage(pkgName, path string, binDir string) error {
 	if err := cmd.Run(); err != nil {
 		return err
 	}
+	baseName := filepath.Base(pkgName)
+	symlink := filepath.Join(GOBIN, baseName)
+
+	prompt("Symlink %v", symlink)
 	// for linux and darwin
-	os.Remove(filepath.Join(GOBIN, pkgName))
-	//return os.Symlink(filepath.Join("../opt/", pkgName, pkgName), getInsPath("bin", pkgName))
-	return os.Symlink(getInsPath("opt", pkgName, pkgName), filepath.Join(GOBIN, pkgName))
+	os.Remove(symlink)
+	return os.Symlink(getInsPath("opt", pkgName, pkgName), symlink)
+}
+
+func prompt(format string, args ...interface{}) {
+	format = "==> " + strings.TrimPrefix(format, "\n") + "\n"
+	fmt.Printf(format, args...)
 }
 
 func InstallAction(c *cli.Context) {
@@ -90,15 +108,23 @@ func InstallAction(c *cli.Context) {
 	if len(c.Args()) < 1 {
 		log.Fatal("Need at lease one argument")
 	}
-	log.Println(GOBIN)
+	// log.Println(GOBIN)
 	pkgName := c.Args().First()
+	// TODO: use myname for now
+	if len(strings.Split(pkgName, "/")) == 1 {
+		pkgName = "codeskyblue/" + pkgName
+	}
+
+	prompt("Repository %v", pkgName)
 	dest, err := downloadSource(pkgName)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	err = deployPackage(pkgName, dest, GOBIN)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Program [%s] installed\n", pkgName)
+
+	prompt("Program [%s] installed", pkgName)
 }
